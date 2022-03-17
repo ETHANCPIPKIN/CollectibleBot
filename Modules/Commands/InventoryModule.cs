3fe3ea3af9fa89c19688c80bab343685e1019d78
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2022-03-16 23:39:09 -0700

    Finished Inventory

---
+++ Modules/Commands/InventoryModule.cs
@@ -0,0 +1,66 @@
+﻿using CollectibleBot.Data.Models;
+using CollectibleBot.Modules.Utils;
+using Discord;
+using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectibleBot.Modules.Commands
+{
+	public class InventoryModule : InteractionModuleBase
+	{
+		private readonly DbUtil _util;
+
+		public InventoryModule(DbUtil util)
+		{
+			_util = util;
+		}
+
+		[SlashCommand("inventory", "Bring up your inventory")]
+		public async Task inventory()
+		{
+			User user = await _util.getUserAsync(Context.User.Id.ToString(), Context.Guild.Id.ToString());
+
+			EmbedBuilder embed = new EmbedBuilder
+			{
+				Author = new EmbedAuthorBuilder
+				{
+					IconUrl = Context.User.GetAvatarUrl() ?? Context.User.GetDefaultAvatarUrl(),
+					Name = Context.User.Username
+				},
+				Title = $"{Context.User.Username}'s Items \n${user.coins} | {user.items.Count} Items",
+				Description = "Use ``/market sell`` to sell an item based on its current market value\n" +
+				"Use ``/market buy`` to access the Auction House to see any available listings.",
+				ThumbnailUrl = Context.User.GetAvatarUrl()
+			};
+
+			if (user.items.Count < 1)
+			{
+				embed.AddField(new EmbedFieldBuilder
+				{
+					Name = "No items!",
+					Value = "Chat for a bit until an item drops, or see if there is any on the Auction House!"
+				});
+				await RespondAsync(embed: embed.Build(), ephemeral: true);
+				return;
+			}
+			
+			foreach (Item item in user.items)
+			{
+				Market market = await _util.getMarketValueAsync(item.Name, Context.Guild.Id.ToString());
+				Collectible c = _util.findItem(item.Name, Context.Guild.Id.ToString());
+				embed.AddField(new EmbedFieldBuilder
+				{
+					Name = $"{Item.rarityToString(item.Rarity)} {item.Name}",
+					Value = $"Current Market Price: {market.price}\n" +
+					$"Estimated Value: {item.Price * c.getRarityMult(item.Rarity)}"
+				});
+			}
+
+			await RespondAsync(embed: embed.Build(), ephemeral: true);
+		}
+	}
+}
